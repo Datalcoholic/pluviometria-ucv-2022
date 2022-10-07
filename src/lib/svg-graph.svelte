e911463e789aab20bea9cdb1074c1c1e854e8d38
R.E <rechezuria@gmail.com>
2022-10-07 17:07:06 -0400

    refactor: :recycle: Pass the element size to svg component

--- src/lib/svg-graph.svelte
+++ src/lib/svg-graph.svelte
@@ -1,12 +1,14 @@
 <script>
 	import Svg from './svg.svelte';
+	import { svgContainerSize } from '../stores/appStores';
+	$: width = $svgContainerSize?.width;
+	$: height = $svgContainerSize?.height;
 </script>
 
-<Svg width="900" height="800">
+<Svg {width} {height}>
 	<!-- test -->
 </Svg>
 
 <!-- markup (zero or more items) goes here -->
-
 <style>
 </style>
