69a0814dd8f0086542821737680eea416f6f8492
R.E <rechezuria@gmail.com>
2022-10-07 15:21:27 -0400

    style: :lipstick: Create Basic layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script>
-	import svelteLogo from './assets/svelte.svg';
 </script>
 
-<main>
+<main class="main">
 	<h1>Title</h1>
+	<article class="svg-container">
+		<!-- svg -->
+	</article>
 </main>
 
 <style>
+	.main {
+		display: grid;
+		justify-content: center;
+		align-items: center;
+	}
+	.svg-container {
+		width: 800px;
+		height: 900px;
+		outline: solid tomato;
+	}
 </style>
