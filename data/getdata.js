1fb1bae055f9e7a55d84369a90dc46bb73a0dcef
R.E <rechezuria@gmail.com>
2022-10-10 16:56:00 -0400

    feat: save 2022 year data

--- data/getdata.js
+++ data/getdata.js
@@ -1,24 +1,40 @@
 import * as d3 from 'd3';
+
 import { readFile, writeFile } from 'fs';
 
 let rawData = await readFile('../data/Maximos.csv', 'utf-8', (err, data) => {
 	if (err) throw err;
 	let resp = data.split('\n');
 	const [dia, mes, año, mm] = resp[0].split(',').map((d) => d.toLowerCase());
+	resp.filter((d, i) => i > 0);
 	resp = resp.map((d, i) => {
-		if (i !== 0) {
-			const [di, m, a, value] = d.split(',');
-			return {
-				date: `${di}-${m}-${a}`,
-				mm: +value,
-			};
-		}
+		const [di, m, a, value] = d.split(',');
+		return {
+			date: new Date(`${m}-${di}-${a}`),
+			day: +di,
+			month: +m,
+			year: +a,
+			mm: +value,
+		};
 	});
-	let toSave = JSON.stringify(resp);
-	console.log('resp :>> ', resp);
-	writeFile('test.json', toSave, (err) => {
+	let currentYearInterval = {
+		start: new Date('01-01-2022'),
+		end: new Date('31-12-2022'),
+	};
+	let thisYear2022 = resp.filter(
+		(d) =>
+			d.date >= currentYearInterval.start || d.date <= currentYearInterval.end
+	);
+	thisYear2022 = Array.from(
+		d3.group(thisYear2022, (m) => m.month),
+		([month, monthlyData]) => ({
+			month,
+			monthlyData,
+		})
+	);
+	//console.log('resp :>> ', thisYear2022);
+	let toSave = JSON.stringify(thisYear2022);
+	writeFile('year2022.json', toSave, (err) => {
 		if (err) throw err;
 	});
 });
-
-console.log('rawData :>> ', rawData);
