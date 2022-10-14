9ec98eab58f4b3fd6a5be4f7f8d516b34cedb154
R.E <rechezuria@gmail.com>
2022-10-14 12:04:02 -0400

    feat: :sparkles: Add rainData2022

--- src/lib/svg-graph.svelte
+++ src/lib/svg-graph.svelte
@@ -5,7 +5,16 @@
 	$: width = $svgContainerSize?.width;
 	$: height = $svgContainerSize?.height;
 
-	$: console.log('year2022 :>> ', $year2022);
+	let rainData2022 = $year2022?.map((d) => {
+		let data = d.monthlyData;
+		data = data.map((d) => {
+			let { date, day, month, year, mm } = d;
+			return { date: new Date(date), day, month, year, mm };
+		});
+		return data;
+	});
+	//console.log('rainData2022 :>> ', rainData2022);
+	console.log('year2022 :>> ', $year2022);
 </script>
 
 <Svg {width} {height}>
