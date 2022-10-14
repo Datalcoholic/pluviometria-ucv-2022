8ca70e91774b5a9349c3fbcacfb4e393a078a370
R.E <rechezuria@gmail.com>
2022-10-14 17:30:06 -0400

    feat: :sparkles: Add dayScale, monthScale and fill scale

--- src/lib/svg-graph.svelte
+++ src/lib/svg-graph.svelte
@@ -2,8 +2,15 @@
 	import Svg from './svg.svelte';
 	import { svgContainerSize } from '../stores/appStores';
 	import { year2022 } from '../stores/dataStore';
+	import * as d3 from 'd3';
 	$: width = $svgContainerSize?.width;
 	$: height = $svgContainerSize?.height;
+
+	//Palette
+	const palette = {
+		indigoDye1: 'hsl(209, 77%, 50%)',
+		indigoDye2: 'hsl(209, 77%, 35%)',
+	};
 
 	let rainData2022 = $year2022?.map((d) => {
 		let data = d.monthlyData;
@@ -13,7 +20,21 @@
 		});
 		return data;
 	});
-	//console.log('rainData2022 :>> ', rainData2022);
+
+	// Scales
+	const dayScale = d3.scaleLinear().domain([1, 31]).range([0, width]);
+	const monthScale = d3
+		.scaleBand()
+		.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+		.range([height, 0]);
+	const mm = rainData2022.flat(1).map((m) => m.mm);
+	const mmExtend = d3.extent(mm);
+	const fillScale = d3
+		.scaleSequential(
+			d3.interpolateRgbBasis([palette.indigoDye2, palette.indigoDye1])
+		)
+		.domain(mmExtend);
+	console.log('fillScale :>> ', fillScale(41), mmExtend, mm);
 	console.log('year2022 :>> ', $year2022);
 </script>
 
