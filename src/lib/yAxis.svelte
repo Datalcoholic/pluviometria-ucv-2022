0a12e815d8a581905b274820cb167b44b823806a
R.E <rechezuria@gmail.com>
2022-10-17 16:06:41 -0400

    refactor: :recycle: pass month names intead month date

--- src/lib/yAxis.svelte
+++ src/lib/yAxis.svelte
@@ -2,12 +2,11 @@
 	import * as d3 from 'd3';
 
 	export let scale, months, x;
-	const monthFormat = d3.timeFormat('%b');
 </script>
 
 <g class="y-axis">
 	{#each months as month}
-		<text {x} y={scale(month)}>{monthFormat(month)}</text>
+		<text {x} y={scale(month)}>{month}</text>
 	{/each}
 </g>
 
