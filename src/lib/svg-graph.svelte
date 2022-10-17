0f711a08528e5f82f005f79614dc5a10840eb242
R.E <rechezuria@gmail.com>
2022-10-17 13:06:36 -0400

    feat: :sparkles: Add yAxis component

--- src/lib/svg-graph.svelte
+++ src/lib/svg-graph.svelte
@@ -1,8 +1,57 @@
 <script>
 	import Svg from './svg.svelte';
+	import { startOfMonth } from 'date-fns';
 	import { svgContainerSize } from '../stores/appStores';
 	import { year2022 } from '../stores/dataStore';
 	import * as d3 from 'd3';
+	import YAxis from './yAxis.svelte';
+	const localeEs = {
+		dateTime: '%A, %e de %B de %Y, %X',
+		date: '%d/%m/%Y',
+		time: '%H:%M:%S',
+		periods: ['AM', 'PM'],
+		days: [
+			'domingo',
+			'lunes',
+			'martes',
+			'miércoles',
+			'jueves',
+			'viernes',
+			'sábado',
+		],
+		shortDays: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+		months: [
+			'enero',
+			'febrero',
+			'marzo',
+			'abril',
+			'mayo',
+			'junio',
+			'julio',
+			'agosto',
+			'septiembre',
+			'octubre',
+			'noviembre',
+			'diciembre',
+		],
+		shortMonths: [
+			'Ene',
+			'Feb',
+			'Mar',
+			'Abr',
+			'May',
+			'Jun',
+			'Jul',
+			'Ago',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dic',
+		],
+	};
+	d3.timeFormatDefaultLocale(localeEs);
+	d3.formatDefaultLocale(localeEs);
+
 	$: width = $svgContainerSize?.width;
 	$: height = $svgContainerSize?.height;
 
@@ -20,15 +69,24 @@
 		});
 		return data;
 	});
+	console.log('rainData2022 :>> ', rainData2022);
+	const margin = { top: 35, right: 20, bottom: 15, left: 15 };
 
 	// Scales
 	//Days
 	const dayScale = d3.scaleLinear().domain([1, 31]).range([0, width]);
 	//Month
-	const monthScale = d3
+	const months = rainData2022.map((d) => {
+		const firstDateOfTheMonth = d[0].date;
+		return startOfMonth(firstDateOfTheMonth);
+	});
+	console.log('months :>> ', months);
+	//[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	$: monthScale = d3
 		.scaleBand()
-		.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-		.range([height, 0]);
+		.domain(months)
+		.range([margin.top, height - margin.bottom]);
+
 	//Fill
 	const mm = rainData2022.flat(1).map((m) => m.mm);
 	const mmExtend = d3.extent(mm);
@@ -37,11 +95,10 @@
 			d3.interpolateRgbBasis([palette.indigoDye2, palette.indigoDye1])
 		)
 		.domain(mmExtend);
-	console.log('year2022 :>> ', $year2022);
 </script>
 
 <Svg {width} {height}>
-	<!-- test -->
+	<YAxis scale={monthScale} {months} x={margin.right} />
 </Svg>
 
 <!-- markup (zero or more items) goes here -->
---
+++ src/lib/yAxis.svelte
@@ -0,0 +1,18 @@
+<script>
+	import * as d3 from 'd3';
+
+	export let scale, months, x;
+	const monthFormat = d3.timeFormat('%b');
+</script>
+
+<g class="y-axis">
+	{#each months as month}
+		<text {x} y={scale(month)}>{monthFormat(month)}</text>
+	{/each}
+</g>
+
+<style>
+	.y-axis {
+		fill: currentColor;
+	}
+</style>
