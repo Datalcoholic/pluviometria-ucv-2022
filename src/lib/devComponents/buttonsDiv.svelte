d3bca883b4705dddbf0182e763a75700627090fa
R.E <rechezuria@gmail.com>
2022-10-19 13:29:12 -0400

    feat: :sparkles: test the filter data

---
+++ src/lib/devComponents/buttonsDiv.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let onclick;
+</script>
+
+<section class="buttons-container">
+	<button on:click={onclick}>Rainy Days</button>
+	<button>Cons Rainy Days</button>
+</section>
+
+<style>
+	.buttons-container {
+		display: flex;
+	}
+</style>
