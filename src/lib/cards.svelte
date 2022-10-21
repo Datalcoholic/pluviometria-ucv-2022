2cb381d58bae5ba781a95aebbc5805f5fcd495c1
R.E <rechezuria@gmail.com>
2022-10-21 11:19:46 -0400

    style: :lipstick: Card style
    add backgound color, border., set font weight and width card

--- src/lib/cards.svelte
+++ src/lib/cards.svelte
@@ -8,12 +8,14 @@
 
 <style>
 	.card {
-		background-color: var(--black-coral-1);
+		background-color: var(--black-coral-2);
+		border: solid 1.7px var(--black-coral-3);
+		border-radius: 6px;
 		line-height: 1.5rem;
-		font-size: 1.25rem;
-		width: 55ch;
+		font-size: 1.18rem;
+		font-weight: 300;
+		width: 40ch;
 		padding: 0.8em;
 		margin: 2em 0;
-		border-radius: 8px;
 	}
 </style>
