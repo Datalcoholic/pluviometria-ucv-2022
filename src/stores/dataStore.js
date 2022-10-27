04f5826c94d2e1fde9dfb3444bcb0a1f69a0c1ec
R.E <rechezuria@gmail.com>
2022-10-27 09:42:09 -0400

    feat: :sparkles: Add previus year data

--- src/stores/dataStore.js
+++ src/stores/dataStore.js
@@ -2,11 +2,17 @@ import { writable } from 'svelte/store';
 import { json } from 'd3';
 
 const year2022 = writable();
+const prevYears = writable();
 
 const data2022 = await json(
 	'https://raw.githubusercontent.com/Datalcoholic/pluviometria-ucv-2022/master/data/year2022.json'
 );
 
-await year2022.set(data2022);
+const prev = await json(
+	'https://raw.githubusercontent.com/Datalcoholic/pluviometria-ucv-2022/master/data/statisticsByYears.json'
+);
 
-export { year2022 };
+await year2022.set(data2022);
+await prevYears.set(prev);
+
+export { year2022, prevYears };
