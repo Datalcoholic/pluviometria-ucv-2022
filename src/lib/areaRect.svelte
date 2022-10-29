15f0ab6a4f5918a2899101d8ab2d6f8c00b372c5
R.E <rechezuria@gmail.com>
2022-10-29 16:49:38 -0400

    feat: :sparkles: Add enter animation

--- src/lib/areaRect.svelte
+++ src/lib/areaRect.svelte
@@ -1,7 +1,7 @@
 <script>
 	import * as d3 from 'd3';
-	export let data, monthScale, dayScale, format, rectMargin;
-	console.log('data :>> ', data);
+	import gsap from 'gsap';
+	export let data, monthScale, dayScale, format;
 
 	const dataArea = data.map((month, i) => {
 		const min = d3.min(month, (d) => d.indexDay);
@@ -21,8 +21,6 @@
 
 	let rectWidth = 30;
 
-	console.log('dataArea :>> ', dataArea);
-
 	function areaPath(start, end, height, r) {
 		const width = end - start + 30;
 		const path = d3.path();
@@ -40,16 +38,44 @@
 		path.closePath();
 		return path.toString();
 	}
+
+	function areaRectTransition(node, { delay = 0 }) {
+		const length = node.getTotalLength();
+		const factor = 150;
+		gsap
+			.fromTo(
+				node,
+				{
+					strokeDasharray: `0 ${length}`,
+					strokeDashoffset: factor, //`${length * factor}`,
+				},
+				{
+					duration: 0.95,
+					keyframes: {
+						'100%': {
+							strokeDasharray: `${length} 0`,
+							strokeDashoffset: factor + 50, //`${length * factor + 50}`,
+						},
+					},
+				}
+			)
+			.delay(delay);
+
+		return {
+			delay,
+		};
+	}
 </script>
 
 <g class="area-rect">
-	{#each dataArea as month}
+	{#each dataArea as month, i}
 		<path
 			d={areaPath(dayScale(month.min), dayScale(month.max), rectWidth, 8)}
 			fill="none"
 			stroke="var(--sandy-brown-2)"
 			style="translate:{dayScale(1) - 15}px 
       {monthScale(month.m) - rectWidth / 2}px"
+			in:areaRectTransition={{ delay: i * 0.35 }}
 		/>
 	{/each}
 </g>
