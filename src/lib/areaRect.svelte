ef4ed5c91be3eaa047e22cbdfb61f18f0aaced2f
R.E <rechezuria@gmail.com>
2022-11-01 16:13:26 -0400

    refactor: :recycle: move areaPath and triangle function to utilities.js

--- src/lib/areaRect.svelte
+++ src/lib/areaRect.svelte
@@ -3,6 +3,7 @@
 	import { scale } from 'svelte/transition';
 	import PrevDaysMarker from './prevDaysMarker.svelte';
 	import RainyDaysMarker from './rainyDaysMarker.svelte';
+	import { areaPath, triangle } from '../Utilities/utils';
 	export let data, data2, monthScale, dayScale, meanScale, format;
 	const numFormat = d3.format('.1f');
 	const dataArea = data.map((month, i) => {
@@ -36,48 +37,6 @@
 
 	let rectWidth = 30;
 	let rectWidthPreV = 40;
-
-	function areaPath(start, end, height, r) {
-		const width = end - start;
-		const path = d3.path();
-		const x = 0;
-		const y = 0;
-		path.moveTo(x + width / 2, y);
-		path.lineTo(width - width / 2, y);
-		path.arcTo(width, y, width, height, r);
-		path.lineTo(width, height / 2);
-		path.arcTo(width, height, width / 2, height, r);
-		path.lineTo(x + width / 2, height);
-		path.arcTo(x, height, x, height / 2, r);
-		path.lineTo(x, height / 2);
-		path.arcTo(x, y, x + height / 2, y, r);
-		path.closePath();
-		return path.toString();
-	}
-	function triangle(start, end, y, tWidth, height, r) {
-		const width = end - start;
-		const path = d3.path();
-		const gap = 10;
-		const x = gap + width;
-		//const y = 0;
-		path.moveTo(x, y + height * 0.5);
-		path.lineTo(x, y + height * 0.5);
-		path.arcTo(x, y + height, x + tWidth * 0.7, y + height - height / 2, r);
-		path.lineTo(x + tWidth * 0.65, y + height * 0.65);
-		path.arcTo(
-			x + tWidth,
-			y + height * 0.5,
-			x + tWidth * 0.05,
-			y + height * 0.05,
-			r
-		);
-		path.lineTo(x + tWidth * 0.3, y + height * 0.15);
-		path.arcTo(x, y, x, y + height * 0.35, r);
-		path.lineTo(x, y + height * 0.4);
-		path.closePath();
-
-		return path.toString();
-	}
 </script>
 
 <g class="area">
