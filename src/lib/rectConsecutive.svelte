d290583dc7329b68debfd70693a0aeb2fd214cf6
R.E <rechezuria@gmail.com>
2022-11-02 13:11:47 -0400

    refactor: :recycle: Teak the enter animation

--- src/lib/rectConsecutive.svelte
+++ src/lib/rectConsecutive.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { areaPath } from '../Utilities/utils';
+	import { areaPath, triangle } from '../Utilities/utils';
 	import * as d3 from 'd3';
 	import gsap from 'gsap';
 	export let data, xScale, yScale;
@@ -16,7 +16,7 @@
 			const path = areaPath(
 				xScale(+dayFormat(start)),
 				xScale(+dayFormat(end)),
-				45,
+				30,
 				5,
 				10
 			);
@@ -35,14 +35,19 @@
 		return cons.map((d) => d);
 	});
 	console.log('consecutiveData :>> ', consecutiveData);
-	function scaleFromCenter(node, { delay = 0 }) {
+	function enter(node, { delay = 0 }) {
 		// gsap.from(node, { scaleX: 0, duration: 2 }).delay(delay);
-
+		const [path, triangle, amount, unit] = node.querySelectorAll([
+			'path',
+			'.triangle',
+			'.amount',
+			'.unit',
+		]);
 		const tl = gsap.timeline({ ease: 'expo.in' });
-		const length = node.getTotalLength();
+		const length = path.getTotalLength();
 
 		tl.fromTo(
-			node,
+			path,
 			{ strokeDasharray: `0 ${length / 2}`, strokeDashoffset: length / 2 },
 			{
 				duration: 0.7,
@@ -50,26 +55,81 @@
 				strokeDashoffset: `${length / 2}`,
 			}
 		)
-			.from(node, { fillOpacity: 0, duration: 0.9 })
+			.from(path, { fillOpacity: 0, duration: 0.9 })
 			.delay(delay);
+		if (triangle) {
+			tl.add(
+				gsap.from(triangle, {
+					y: function (i, t, ts) {
+						return t.getBoundingClientRect().y - 10;
+					},
+					opacity: 0,
+				})
+			);
+			tl.add(
+				gsap.from([amount, unit], {
+					y: -10,
+					opacity: 0,
+				})
+			);
+		}
+
 		return { delay };
 	}
 </script>
 
-<g class="consecutive-paths">
-	{#each consecutiveData.flat(1) as month, i}
+{#each consecutiveData.flat(1) as month, i}
+	<g class="consecutive-paths" in:enter={{ delay: i * 0.08 }}>
 		<path
-			in:scaleFromCenter={{ delay: i * 0.08 }}
 			d={month.path}
-			style="translate:{xScale(month.day)}px {yScale(month.month) - 45 / 2}px"
+			style="translate:{xScale(month.day)}px {yScale(month.month) - 30 / 2}px"
 			fill={d3.rgb('hsl(28, 83%, 81%)').copy({ opacity: month.opacity })}
 			stroke="var(--sandy-brown-2)"
 		/>
-	{/each}
-</g>
+		{#if month.delta === 7}
+			<path
+				class="triangle"
+				d={triangle(0, 0, 0, 20, 15, 2)}
+				style="translate:{xScale(month.day) -
+					20 +
+					xScale(month.delta - 1) / 2 -
+					15}px {yScale(month.month) + 15 + 10}px; rotate:{90}deg"
+			/>
+			<text
+				class="amount"
+				x={xScale(month.day) - 20 + xScale(month.delta - 1) / 2 - 15}
+				y={yScale(month.month) + 15 + 10 + 20 + 18}>{month.delta}</text
+			>
+			<text
+				class="unit"
+				x={xScale(month.day) - 20 + xScale(month.delta - 1) / 2 - 15 + 15}
+				y={yScale(month.month) + 15 + 10 + 20 + 18}>dias</text
+			>
+		{/if}
+	</g>
+{/each}
 
 <style>
 	path {
 		transform-origin: center;
+	}
+	.triangle {
+		transform-origin: center center;
+		transform-box: fill-box;
+		stroke: var(--sandy-brown-2);
+		fill: none;
+	}
+
+	.amount {
+		font-size: 1.5rem;
+		font-weight: 400;
+		stroke: var(--orange-soda);
+		fill: var(--sandy-brown-2);
+		stroke-width: 1px;
+		paint-order: stroke;
+	}
+	.unit {
+		font-size: 0.98rem;
+		fill: var(--sandy-brown-2);
 	}
 </style>
