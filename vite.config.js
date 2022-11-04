57ce3a69fa6efe170bc5cd526bdb4eabc70ed369
R.E <rechezuria@gmail.com>
2022-11-04 17:41:14 -0400

    Biuld: Does not work

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
-	build: { target: 'es2017' },
-	optimizeDeps: {
-		esbuildOptions: { target: 'es2017', supported: { bigint: true } },
-	},
 });
