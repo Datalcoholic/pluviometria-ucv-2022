537e3c305a1c9e7f8d88f4c6a19c0bb7d150c9bd
R.E <rechezuria@gmail.com>
2022-11-04 17:32:32 -0400

    Biuld: And now this config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
-	esbuild: {
-		target: 'es2020',
+	build: { target: 'es2020' },
+	optimizeDeps: {
+		esbuildOptions: { target: 'es2020', supported: { bigint: true } },
 	},
 });
