6dcf5197734730a588a9a731a030e78cdbd021d8
R.E <rechezuria@gmail.com>
2022-11-05 11:12:40 -0400

    Biuld: And Now!

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
 	plugins: [svelte()],
 	build: {
-		target: 'esnext',
+		target: 'node18.7',
 		module: 'esm',
 	},
 });
