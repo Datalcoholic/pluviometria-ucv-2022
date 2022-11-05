673d95c626dfca1ddd0e7825dfa6d6bdb6d88de0
R.E <rechezuria@gmail.com>
2022-11-05 11:08:40 -0400

    Build What about now!

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
 	plugins: [svelte()],
 	build: {
-		target: 'es2022',
+		target: 'esnext',
+		module: 'esm',
 	},
 });
