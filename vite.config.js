172f51fd28ee982f1ec54e79a77d9530db4f8a10
R.E <rechezuria@gmail.com>
2022-11-05 22:29:50 -0400

    Build: What about this!

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
 	plugins: [svelte()],
 	build: {
-		target: ['edge90', 'chrome90', 'firefox90', 'safari15', 'esnext'],
+		target: ['edge90', 'chrome90', 'firefox90', 'safari15'],
 	},
 });
