1585c876d29a5241a50b501617935b45ff39314c
R.E <rechezuria@gmail.com>
2022-11-07 10:39:31 -0400

    refactor: :recycle: Render cards with cat colors

--- src/lib/cards.svelte
+++ src/lib/cards.svelte
@@ -1,9 +1,62 @@
 <script>
-	export let text, cardsRef, i;
+	export let cardsRef;
 </script>
 
-<div class="card {text.name}" bind:this={cardsRef[i]}>
-	<p>{text.text}</p>
+<div class="card card-1" bind:this={cardsRef[0]}>
+	<p>
+		Para este <strong class="this-year">2022</strong> las lluvias en Caracas se han
+		distribuido de la siguiente manera:
+	</p>
+</div>
+<div class="card card-2" bind:this={cardsRef[1]}>
+	<p>
+		Los días que se registraron mayor cantidad de lluvias se fueron el
+		<strong class="this-year"> 22 de Abril </strong>,
+		<strong class="this-year"> 26 de Agosto </strong> y
+		<strong class="this-year"> 26 de Septiembre </strong>
+	</p>
+</div>
+<div class="card card-3" bind:this={cardsRef[2]}>
+	<p>
+		En contraste los días con la mayor cantidad de lluvia registrados
+		históricamente fueron el
+		<strong class="prev-year"> 17 de Noviembre de 2008 </strong>,
+		<strong class="prev-year"> 22 de Octubre de 1994 </strong> y el
+		<strong class="prev-year"> 2 de Septiembre de 2020 </strong>
+	</p>
+</div>
+<div class="card card-4" bind:this={cardsRef[3]}>
+	<p>
+		Para este
+		<strong class="this-year"> 2022 </strong> tenemos que
+		<strong class="this-year"> Agosto </strong> fue el mes con más días
+		lluviosos con
+		<strong class="this-year"> 1,6 </strong>
+		veces sobre el
+		<strong class="prev-year"> promedio </strong> histórico, seguido por
+		<strong class="this-year"> Junio </strong> con
+		<strong class="this-year"> 1,4 </strong> veces sobre el
+		<strong class="prev-year"> promedio </strong> y
+		<strong class="this-year"> Septiembre </strong> con
+		<strong class="this-year"> 1,4 </strong> veces sobre el
+		<strong class="prev-year"> promedio </strong>. Destaca
+		<strong class="this-year"> Marzo </strong> el cual registró
+		<strong class="this-year"> 2 </strong> veces más días lluviosos sobre el
+		<strong class="prev-year"> promedio </strong>
+	</p>
+</div>
+<div class="card card-5" bind:this={cardsRef[4]}>
+	<p>
+		No solo los días de lluvias y la cantidad de la misma son factores a tomar
+		en cuenta, también se toman los días consecutivos de lluvias, ya que a más
+		días consecutivos lloviendo mayor es la propensión de los suelos a
+		saturarse. Este año el mes con mayor días continuos de lluvias fue <strong
+			class="this-year"
+		>
+			Agosto
+		</strong>
+		con <strong class="this-year"> 7 </strong> días
+	</p>
 </div>
 
 <style>
@@ -19,4 +72,10 @@
 		margin: 20em 0 0em;
 		opacity: 0;
 	} */
+	.this-year {
+		color: var(--sandy-brown-2);
+	}
+	.prev-year {
+		color: var(--indigo-dye-4);
+	}
 </style>
--- src/lib/scrolleCards.svelte
+++ src/lib/scrolleCards.svelte
@@ -2,40 +2,14 @@
 	import Cards from './cards.svelte';
 	import { cardsStored } from '../stores/appStores';
 
-	// CARDS
-	const cards = [
-		{
-			name: 'card-1',
-			text: 'Para este 2022 las lluvias en Caracas se han distribuido de la siguiente manera: ',
-		},
-
-		{
-			name: 'card-2',
-			text: 'Los días que se registraron mayor cantidad de lluvias se fueron el 22 de Abril, 26 de Agosto y 26 de Septiembre ',
-		},
-		{
-			name: 'card-3',
-			text: 'En contraste los días con la mayor cantidad de lluvia registrados históricamente fueron el 17 de Noviembre de 2008, 22 de Octubre de 1994 y el 2 de Septiembre de 2020',
-		},
-
-		{
-			name: 'card-4',
-			text: 'Para este 2022 tenemos que Agosto fue el mes con más días lluviosos con 1,6 veces sobre el promedio histórico, seguido por Junio con 1,4 veces sobre el promedio y Septiembre con 1,4 veces sobre el promedio. Destaca Marzo el cual registró 2 veces más días lluviosos sobre el promedio',
-		},
-		{
-			name: 'card-5',
-			text: 'No solo los días de lluvias y la cantidad de la misma son factores a tomar en cuenta, también se toman los días consecutivos de lluvias, ya que a más días consecutivos lloviendo mayor es la propensión de los suelos a saturarse. Este año el mes con mayor días continuos de lluvias fue Agosto con 7 días',
-		},
-	];
-
 	let cardsRef = [];
 	cardsStored.set(cardsRef);
 </script>
 
 <section class="cards-container">
-	{#each cards as card, i (card.name)}
-		<Cards text={card} {cardsRef} {i} />
-	{/each}
+	<!-- {#each cards as card, i (card.name)} -->
+	<Cards {cardsRef} />
+	<!-- {/each} -->
 </section>
 
 <style>
